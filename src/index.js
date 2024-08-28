4f63e9a308a90df4506ca1ecf6360d02ecadefc6
AcquziaCooper <lungiekunene@gmail.com>
2024-08-28 15:03:44 +0200

    Added basic form handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["When the mountain calls"],
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let poemElement = document.querySelector("#poem-gen");
+poemElement.addEventListener("submit", generatePoem);
